2e05b389a2155ae3859ef77abbc76046504d6a52
yousefiraq <50072463+yousefiraq@users.noreply.github.com>
2025-03-12 01:27:03 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,7 +11,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBf9FQ...", // استبدل بمعلومات مشروعك
+    apiKey: "AIzaSyBf9FQ...", // استبدل بمفتاحك
     authDomain: "gaz-delivery-1.firebaseapp.com",
     projectId: "gaz-delivery-1",
     storageBucket: "gaz-delivery-1.appspot.com",
